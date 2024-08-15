7f6ecac590b989cdc336f3040e7e269e258330e4
René Pacios <rpacios@webrene.es>
2024-08-15 22:42:03 +0200

    Create default interfaces for classic version

---
+++ src/Rene.Utils.Db.Abstractions/IDbUtilsUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rene.Utils.Db
+{
+    public interface IDbUtilsUnitOfWork
+    {
+        int SaveChanges();
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
