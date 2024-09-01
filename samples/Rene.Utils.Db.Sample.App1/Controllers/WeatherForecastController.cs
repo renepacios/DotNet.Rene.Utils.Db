807a1f89fc73f19008fa43bfc66d04528d897ffa
René Pacios <rpacios@webrene.es>
2024-09-01 12:52:09 +0200

    implementing crud samples

--- samples/Rene.Utils.Db.Sample.App1/Controllers/WeatherForecastController.cs
+++ samples/Rene.Utils.Db.Sample.App1/Controllers/WeatherForecastController.cs
@@ -25,15 +25,23 @@ namespace Rene.Utils.Db.Sample.App1.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IActionResult> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = Random.Shared.Next(-20, 55),
+            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+
+            return Ok(await _sender.Send(new GetAllCommand<WeatherForecastViewModel>()));
+        }
+
+        [HttpGet("paginated", Name = "GetWeatherForecastPaginated")]
+        public async Task<IDbUtilsPaginatedData<WeatherForecastViewModel>> GetPaginated([FromQuery] int page = 0, [FromQuery] int size = 10)
+        {
+           return await _sender.Send(new GetPaginatedCommand<WeatherForecastViewModel, WeatherForecast>(new DbUtilsAllSpecification<WeatherForecast>(), size, page));
         }
 
         [HttpPost(Name = "PostWeatherForecast")]
@@ -42,5 +50,20 @@ namespace Rene.Utils.Db.Sample.App1.Controllers
             var result = await _sender.Send(new AddCommand<WeatherForecastViewModel>(viewModel));
             return Created(string.Empty, result);
         }
+
+        [HttpPut("{id}", Name = "PutWeatherForecast")]
+        public async Task<IActionResult> Put(int id, WeatherForecastViewModel viewModel)
+        {
+            var result = await _sender.Send(new UpdateCommand<WeatherForecastViewModel>(viewModel, id));
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}", Name = "DeleteWeatherForecast")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _sender.Send(new DeleteCommand<WeatherForecastViewModel>(id));
+            return Ok(result);
+        }
+
     }
 }
---
+++ src/Rene.Utils.Db.Abstractions/DbUtilsAllSpecification.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rene.Utils.Db
+{
+    using System.Linq.Expressions;
+
+    public sealed class DbUtilsAllSpecification<T> : IDbUtilsSpecification<T>
+    {
+        public DbUtilsAllSpecification()
+        {
+
+        }
+
+        public DbUtilsAllSpecification(ICollection<KeyValuePair<bool, Expression<Func<T, object>>>> sortSequence)
+        {
+            OrderBy = sortSequence
+                .Select(s => new KeyValuePair<bool, Expression<Func<T, object>>>(s.Key, s.Value))
+                .ToList();
+        }
+
+
+        public Expression<Func<T, bool>> Criteria { get; }
+        public ICollection<Expression<Func<T, bool>>> Criterias { get; }
+        public List<Expression<Func<T, object>>> Includes { get; }
+        public List<string> IncludeStrings { get; }
+        public List<KeyValuePair<bool, Expression<Func<T, object>>>> OrderBy { get; }
+        public Expression<Func<T, object>> GroupBy { get; }
+        public bool IgnoreQueryFilters { get; }
+        public bool AsNoTracking { get; }
+        public bool AsSplitQuery { get; }
+        public int Take { get; }
+        public int Skip { get; }
+        public bool IsPagingEnabled { get; }
+    }
+}
